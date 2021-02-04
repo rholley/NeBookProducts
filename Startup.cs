5921c68d108a44220aa43be6fb437752d9c349de
rholley <rholley.gmt@outlook.com>
2021-02-04 10:20:03 -0500

    Clean up using statements

--- Startup.cs
+++ Startup.cs
@@ -1,12 +1,10 @@
+using NeBookProducts.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
-using Microsoft.EntityFrameworkCore;
-using NeBookProducts.Models;
-
 
 namespace NeBookProducts
 {
